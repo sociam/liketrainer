76219a105a11a0bea9b66c21a214789b04ca3c05
electronic Max <electronic@electronic-mac.home>
2017-04-26 02:15:00 +0100

    chat

--- chat.py
+++ chat.py
@@ -9,15 +9,20 @@ from nltk.corpus import nps_chat
 from collections import Counter
 import make_clf
 
+DEBUG_CLASSIFICATIONS = True
+
 ## if your nltk_data is not in the normal place ... 
 def addpath(p):
 	nltk.data.path.append(p)
+
+not_channel_op = lambda m : not ' '.join(m) in ['PART', 'JOIN']
 
 def process_fid(fid, by_demo):
 	matches = re.search('(\d+)-(\d+)-(.*)_(.*).xml', fid)
 	(mon, day, demo, posts) = (matches.group(1), matches.group(2), matches.group(3), matches.group(4))
 	# print 'month ', mon, ' day ', day, ' demo ', demo, ' posts ', posts
-	by_demo[demo] = (by_demo.get(demo) or []) + nps_chat.posts(fid)[:]
+	by_demo[demo] = (by_demo.get(demo) or []) + filter(not_channel_op, nps_chat.posts(fid)[:])
+
 
 # loads the corpus into a dict by demographic { demo1: [msg1, msg2] }
 def load_by_demo():
@@ -26,11 +31,11 @@ def load_by_demo():
 	return by_demo
 
 # add more paths here, won't hurt if the path doesn't exist on your computer
+# (nltk is a bad boy and likes installing in shit places)
 addpath('/Users/electronic/Desktop/liketrainer/nltk_data')
 
 corpus_by_demographic = load_by_demo()
 # print make_clf.clf.predict(['happy world. nice words. very great.'])
-
 
 joinup = lambda p: ' '.join(p)
 predict = lambda s: make_clf.clf.predict([s])[0]
@@ -41,5 +46,9 @@ predict = lambda s: make_clf.clf.predict([s])[0]
 
 freqs = {}
 for demographic, msgs in corpus_by_demographic.items():
+	if (DEBUG_CLASSIFICATIONS):
+		for m in msgs:
+			print '%s : %s ' % (joinup(m), str(predict(joinup(m))))
+
 	freqs[demographic] = dict(Counter([predict(joinup(x)) for x in msgs]))
 print freqs
